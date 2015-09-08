0f961f5f828e834b84099d68d0df91e61863b2c1
Shelby89 <shelby_dima@mail.ru>
2015-09-08 14:39:24 +0300

    ticket:1 test.py

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Contact
 
 # Create your tests here.
 
@@ -7,7 +8,8 @@ class SomeTests(TestCase):
     #def test_math(self):
     #    "Here goes testing of math function"
     #    assert(2 + 2 == 5)
-	def TestMyContact(self):
+	def test_my_contact(self):
 		# Primary key must be "1" for my contact
-		my_contact = Contact.object.get(pk=1)
-		self.assertEqual(my_contact.last_name, "Sapotnitskiy", "Its not my contacts!")
+		my_contact = Contact.objects.get(pk = 1)
+		my_last_name = my_contact.last_name
+		self.assertEqual(my_last_name, "Sapotnitskiy", "Its not my contacts!")
