1a91e0245ce5952693d80e07dd17f8be37f63520
Shelby89 <shelby_dima@mail.ru>
2015-09-08 14:16:41 +0300

    ticket:1 test.py changed

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -4,6 +4,10 @@ from django.test import TestCase
 
 
 class SomeTests(TestCase):
-    def test_math(self):
-        "Here goes testing of math function"
-        assert(2 + 2 == 5)
+    #def test_math(self):
+    #    "Here goes testing of math function"
+    #    assert(2 + 2 == 5)
+	def TestMyContact(self):
+		# Primary key must be "1" for my contact
+		my_contact = Contact.object.get(pk=1)
+		self.assertEqual(my_contact.last_name, "Sapotnitskiy", "Its not my contacts!")
