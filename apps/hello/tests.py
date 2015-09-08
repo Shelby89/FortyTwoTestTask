969c4cbe71154d680a513a2cc611e702f3b6560c
Shelby89 <shelby_dima@mail.ru>
2015-09-08 14:08:09 +0300

    change test.py

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -4,6 +4,10 @@ from django.test import TestCase
 
 
 class SomeTests(TestCase):
-    def test_math(self):
+    """def test_math(self):
         "put docstrings in your tests"
-        assert(2 + 2 == 5)
+        assert(2 + 2 == 5)"""
+	"""def TestMyContact(self):
+		# Primary key must be "1" for my contact
+		my_contact = Contact.object.get(pk=1)
+		self.assertEqual(my_contact.last_name, "Sapotnitskiy", "Ids are not equal!")"""
