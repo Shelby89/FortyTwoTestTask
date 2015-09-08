7fbdc3ad75388ca01a6cfb302bff795b0f73a088
Shelby89 <shelby_dima@mail.ru>
2015-09-08 17:31:18 +0300

    ticket:1 add test discription

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -9,7 +9,7 @@ class SomeTests(TestCase):
     #    "Here goes testing of math function"
     #    assert(2 + 2 == 5)
 	def test_my_contact(self):
-		" Primary key must be "1" for my contact"
+		" Primary key must be '1' for my contact"
 		my_contact = Contact.objects.get(pk = 1)
 		my_last_name = my_contact.last_name
 		self.assertEqual(my_last_name, "Sapotnitskiy", "Its not my contacts!")
