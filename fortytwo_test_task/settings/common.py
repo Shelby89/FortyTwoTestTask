3524f6efef6dc74749dc3648bad1781cf9811aa9
Shelby89 <shelby_dima@mail.ru>
2015-09-10 21:31:16 +0300

    ticket:1 makefile test pass

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -66,7 +66,6 @@ WSGI_APPLICATION = 'fortytwo_test_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': '../deploy.sqlite3',
         'USER': '',
         'PASSWORD': '',
