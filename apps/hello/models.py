7183d0964194e015989c9ddfefe05acb23fa8427
Shelby89 <shelby_dima@mail.ru>
2015-09-11 02:58:46 +0300

    ticket:1 add admin user

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from django.conf import settings
 from django.contrib.auth import models as auth_models
 from django.contrib.auth.management import create_superuser
@@ -12,14 +10,14 @@ signals.post_syncdb.disconnect(
     dispatch_uid='django.contrib.auth.management.create_superuser')
 
 
-def create_adminuser(app, created_models, verbosity, **kwargs):
+def create_testuser(app, created_models, verbosity, **kwargs):
     if not settings.DEBUG:
         return
     try:
         auth_models.User.objects.get(username='admin')
     except auth_models.User.DoesNotExist:
         print '*' * 80
-        print 'Creating admin user -- login: admin, password: admin'
+        print 'Creating test user -- login: admin, password: admin'
         print '*' * 80
         assert auth_models.User.objects.create_superuser('admin',
                                                          'shelby_dima@mail.ru',
@@ -28,9 +26,9 @@ def create_adminuser(app, created_models, verbosity, **kwargs):
         print 'Admin user already exists.'
 
 signals.post_syncdb.connect(
-    create_adminuser,
+    create_testuser,
     sender=auth_models,
-    dispatch_uid='common.models.create_adminuser')
+    dispatch_uid='common.models.create_testuser')
 
 
 class Contact(models.Model):
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rev_path=`pwd`
-config_path=`dirname ${rev_path}`/uwsgi
-
-if [ ! -f ../deploy.sqlite3 ]
-then
-    PYTHONPATH="${config_path}:${rev_path}" django-admin.py syncdb --noinput --settings settings_deploy
-fi
