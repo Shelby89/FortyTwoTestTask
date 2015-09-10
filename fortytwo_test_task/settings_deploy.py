d11b19c382b41c234fb816c3af7bbacd28ed34ec
Shelby89 <shelby_dima@mail.ru>
2015-09-11 01:05:29 +0300

    ticket:1 css and admin

---
+++ fortytwo_test_task/settings_deploy.py
@@ -0,0 +1,13 @@
+from fortytwo_test_task.settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '../deploy.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
