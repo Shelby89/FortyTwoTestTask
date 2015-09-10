19f85fd4279cbc2119faeeb09145877ac63c542f
Shelby89 <shelby_dima@mail.ru>
2015-09-10 23:01:19 +0300

    ticket:1 admin user

--- post_deploy.sh
+++ post_deploy.sh
@@ -5,6 +5,5 @@ config_path=`dirname ${rev_path}`/uwsgi
 
 if [ ! -f ../deploy.sqlite3 ]
 then
-    PYTHONPATH="${config_path}:${rev_path}" django-admin.py syncdb
---noinput --settings settings_deploy
+    PYTHONPATH="${config_path}:${rev_path}" django-admin.py syncdb --noinput --settings settings_deploy
 fi
