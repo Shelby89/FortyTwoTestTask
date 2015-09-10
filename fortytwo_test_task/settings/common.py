fc7530b32916100cca05a3df7d50c91d4622e0bf
Shelby89 <shelby_dima@mail.ru>
2015-09-11 00:12:07 +0300

    ticket:1 hello assets_3

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -110,13 +110,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/assets /'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	os.path.join(BASE_DIR, 'apps', 'hello', 'assets'),
     os.path.join(BASE_DIR, 'assets'),
 )
 
