bcd17120c0e2c148d65b734f7d5f14a850a5476b
shelby89 <shelbynagliy@gmail.com>
2015-11-06 05:05:14 +0000

    ticket:3 added title refresh

--- apps/list_requests/urls.py
+++ apps/list_requests/urls.py
@@ -3,4 +3,5 @@ from apps.list_requests import views
 
 urlpatterns = patterns('',
     url(r'^$', views.list_requests, name='requests'),
+    (r'^xhr_test/','apps.list_requests.views.xhr_test'),
 )
--- apps/list_requests/views.py
+++ apps/list_requests/views.py
@@ -3,22 +3,30 @@ import datetime
 from django.utils import timezone
 from django.shortcuts import render
 from list_requests.models import StoredRequests
+from django.http import HttpResponse
+
+time_per_session1 = []
 
 def list_requests(request):
-    # print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # print request.META.get('PATH_INFO')
-    # print request.META.get('REQUEST_METHOD')
-    # print request.META.get('SERVER_PROTOCOL')
-    # print '%s  %s', request.META.get('REMOTE_ADDR'), request.META.get('SERVER_PORT')
-    # print "====================="
-    # print request.META.get ('HTTP_X_FORWARDED_FOR')
-    time_per_session = timezone.now()
-    #print time_1
-    #print StoredRequests.objects.last().request_time
+    print time_per_session1
+    if request.is_ajax() == False and request.method == 'GET':
+        data = timezone.now()
+    else:
+        data = None
+        
+    if data != None:
+            time_per_session1.append(data)
+    
+    time_per_session = time_per_session1[-1] 
+    print "Time per session %s" % time_per_session
+    print "IS_AJAX = %s" % request.is_ajax()
+    print "REQUEST METHOD = %s" % request.method
+    print "============================"
+    # print "Time of last req %s" % StoredRequests.objects.last().request_time
     #print StoredRequests.objects.last().request_time <= time_per_session
-    #selected_requests = StoredRequests.objects.filter(request_time__lte=time_per_session).order_by('-request_time')[:10]
-    selected_requests = StoredRequests.objects.order_by('-request_time')[:10]
-    count_req = StoredRequests.objects.all().count
+    selected_requests = StoredRequests.objects.filter(request_time__gte=time_per_session).order_by('-request_time')[:10]
+    #selected_requests = StoredRequests.objects.order_by('-request_time')[:10]
+    count_req = StoredRequests.objects.filter(request_time__gte=time_per_session).count
     return render(request,
                     'list_requests/requests.html',
                         {'selected_requests': selected_requests,
