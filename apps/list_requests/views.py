0d089ab7169ab9015dd59df4b8ce3f41e32fd8c3
shelby89 <shelbynagliy@gmail.com>
2015-11-07 05:27:12 +0000

    ticket:3 changing css

--- apps/list_requests/views.py
+++ apps/list_requests/views.py
@@ -8,7 +8,6 @@ from django.http import HttpResponse
 time_per_session1 = []
 
 def list_requests(request):
-    print time_per_session1
     if request.is_ajax() == False and request.method == 'GET':
         data = timezone.now()
     else:
@@ -17,16 +16,15 @@ def list_requests(request):
     if data != None:
             time_per_session1.append(data)
     
-    time_per_session = time_per_session1[-1] 
-    print "Time per session %s" % time_per_session
-    print "IS_AJAX = %s" % request.is_ajax()
-    print "REQUEST METHOD = %s" % request.method
-    print "============================"
-    # print "Time of last req %s" % StoredRequests.objects.last().request_time
-    #print StoredRequests.objects.last().request_time <= time_per_session
-    selected_requests = StoredRequests.objects.filter(request_time__gte=time_per_session).order_by('-request_time')[:10]
-    #selected_requests = StoredRequests.objects.order_by('-request_time')[:10]
-    count_req = StoredRequests.objects.filter(request_time__gte=time_per_session).count
+    if len(time_per_session1) == 0:
+        time_per_session = None
+        selected_requests = None
+        count_req = 0
+    else:
+        time_per_session = time_per_session1[-1]
+        selected_requests = StoredRequests.objects.filter(request_time__gte=time_per_session).order_by('-request_time')[:10]
+        count_req = StoredRequests.objects.filter(request_time__gte=time_per_session).count
+
     return render(request,
                     'list_requests/requests.html',
                         {'selected_requests': selected_requests,
