a96f17fe09d0899dabeb7ce4b2cf04ddfc992e2e
shelby89 <shelbynagliy@gmail.com>
2015-11-17 12:35:27 +0000

    ticket:3 requests without static

--- apps/list_requests/middleware.py
+++ apps/list_requests/middleware.py
@@ -4,7 +4,7 @@ from list_requests.models import StoredRequests
 class MyHttpRequestMiddleware(object):
     def process_request(self, request):
         if request.is_ajax() is not True:
-            if '/static/' not in request.META.get('PATH_INFO'):
+            if 'static' not in request.META.get('PATH_INFO'):
                 StoredRequests(method = request.META.get('REQUEST_METHOD'),
 	        			    path_info = request.META.get('PATH_INFO'),
 	        				server_protocol = request.META.get('SERVER_PROTOCOL'),
