833bc87038a9d4efe4bf2832d720afd92ad66084
shelby89 <shelbynagliy@gmail.com>
2015-11-17 04:39:43 +0000

    ticket:3 for test some functions

--- apps/list_requests/middleware.py
+++ apps/list_requests/middleware.py
@@ -3,15 +3,11 @@ from list_requests.models import StoredRequests
 
 class MyHttpRequestMiddleware(object):
     def process_request(self, request):
-        if request.is_ajax() is not True:
-            try:
-                req = StoredRequests()
-                req.method = request.META.get('REQUEST_METHOD')
-                req.path_info = request.META.get('PATH_INFO')
-                req.server_protocol = request.META.get('SERVER_PROTOCOL')
-                req.server_port = request.META.get('SERVER_PORT')
-                req.remote_address = request.META.get('REMOTE_ADDR')
-                req.save()
-            except KeyError:
-                pass
+        if not request.is_ajax():
+            if '/static/' not in request.META.get('PATH_INFO'):
+                StoredRequests(method = request.META.get('REQUEST_METHOD'),
+	        			    path_info = request.META.get('PATH_INFO'),
+	        				server_protocol = request.META.get('SERVER_PROTOCOL'),
+	        				server_port = request.META.get('SERVER_PORT'),
+	        				remote_address = request.META.get('REMOTE_ADDR')).save()
         return None
--- apps/list_requests/views.py
+++ apps/list_requests/views.py
@@ -8,17 +8,15 @@ time_per_session1 = []
 def list_requests(request):
     if request.is_ajax() is False and request.method == 'GET':
         time_per_session1.append(StoredRequests.objects.last().request_time)
-    
+
     if len(time_per_session1) == 0:
         time_per_session = None
-        
+        count_req = 0
+        selected_requests = None
     else:
         time_per_session = time_per_session1[-1]
-
-    selected_requests = StoredRequests.objects.order_by('-request_time')[:10]
-    count_req = StoredRequests.objects.filter(request_time__gt=time_per_session).count
-    
-    
+        selected_requests = StoredRequests.objects.order_by('-request_time')[:10]
+        count_req = StoredRequests.objects.filter(request_time__gt=time_per_session).count
     
     return render(
         request,
