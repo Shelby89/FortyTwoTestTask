255b6783a964fb8e808a6311fb3c53496ef7169e
shelby89 <shelbynagliy@gmail.com>
2015-11-21 20:25:49 +0000

    ticket:3 ready for review 5

--- assets/js/ajax.refresh.js
+++ assets/js/ajax.refresh.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
       $.ajax({
          url: '/requests/',
          success: function(data) {
-            //var html = $(data).filter('#refresh').html();
             $('#refresh').html($(data).filter('#refresh').html());
-            //var myTitle = $(data).filter('title').html();
-            //$('title').html(myTitle);
             $('title').html($(data).filter('title').html());
          }
       });
