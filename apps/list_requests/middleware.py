e6077985bf0278b1a943cb251861fc31c8320a96
shelby89 <shelbynagliy@gmail.com>
2016-01-06 14:20:06 +0000

    ticket:3 ready for review 05

--- apps/list_requests/middleware.py
+++ apps/list_requests/middleware.py
@@ -3,12 +3,13 @@ from list_requests.models import StoredRequests
 
 class MyHttpRequestMiddleware(object):
     def process_request(self, request):
-        if (request.is_ajax() is False
-                and 'static' not in request.META.get('PATH_INFO')):
-            StoredRequests(
-                method=request.META.get('REQUEST_METHOD'),
-                path_info=request.META.get('PATH_INFO'),
-                server_protocol=request.META.get('SERVER_PROTOCOL'),
-                server_port=request.META.get('SERVER_PORT'),
-                remote_address=request.META.get('REMOTE_ADDR')
-            ).save()
+        if request.is_ajax() or 'static' in request.META.get('PATH_INFO'):
+            return
+
+        StoredRequests(
+            method=request.META.get('REQUEST_METHOD'),
+            path_info=request.META.get('PATH_INFO'),
+            server_protocol=request.META.get('SERVER_PROTOCOL'),
+            server_port=request.META.get('SERVER_PORT'),
+            remote_address=request.META.get('REMOTE_ADDR')
+        ).save()
