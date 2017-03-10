1381f6402c89545d860b6b745c548a5081e28be1
dasap89 <shelbynagliy@gmail.com>
2017-03-10 16:41:55 +0200

    ticket:3 middleware tested, coded and deployed.

--- apps/list_requests/tests.py
+++ apps/list_requests/tests.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+import time
 
 from apps.list_requests.models import StoredRequests
 
@@ -25,7 +26,7 @@ class Ticket_3_Tests(TestCase):
             server_port="8000",
             remote_address="1.1.1.1"
         )
-        response = self.client.get(reverse('list_requests:requests'))
+        response = self.client.get(reverse('list_requests:table'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "GET")
         self.assertContains(response, "some_url/")
@@ -66,3 +67,41 @@ class Ticket_3_Tests(TestCase):
         self.assertEqual(row.remote_address, "1.1.1.1")
         self.assertNotEqual(row.request_time, None)
         self.assertEqual(row.viewed, False)
+
+    def test_middleware_is_working_and_return_my_request(self):
+        "Check if middleware is working and return new request"
+        response = self.client.get(reverse('list_requests:requests'))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('list_requests:table'))
+        self.assertEqual(response.status_code, 200)
+        content = response.content
+        first_td = content.find("<td>")
+        last_td = content.find("</td>")
+        last1 = content[first_td:last_td]
+        time.sleep(1)
+        response = self.client.get(reverse('list_requests:table'))
+        self.assertEqual(response.status_code, 200)
+        content = response.content
+        first_td = content.find("<td>")
+        last_td = content.find("</td>")
+        last2 = content[first_td:last_td]
+        self.assertNotEqual(last1, last2)
+
+    def test_middleware_doesnt_save_ajax_requests(self):
+        "Middleware must not save ajax requests and requests for staticfiles"
+        StoredRequests.objects.all().delete()
+        self.client.get(reverse('list_requests:requests'),
+                        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        requests = StoredRequests.objects.all().count()
+        self.assertEquals(requests, 0)
+        static_files_req = StoredRequests.objects.filter(
+            path_info__contains='static'
+            ).count()
+        self.assertEquals(static_files_req, 0)
+
+    def test_requests_page_return_ten_requests(self):
+        "View must return no more than 10 requests"
+        for i in range(0, 15):
+            response = self.client.get(reverse('list_requests:requests'))
+            self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['selected_requests'].count(), 10)
