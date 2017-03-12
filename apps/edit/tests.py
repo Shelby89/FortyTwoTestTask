ae27ead998d6d06f421c004a7a05f81699ed8089
dasap89 <shelbynagliy@gmail.com>
2017-03-12 13:30:32 +0200

    ticket:5 mockup and view with hard-coded data

--- apps/edit/tests.py
+++ apps/edit/tests.py
@@ -7,7 +7,28 @@ from apps.list_requests.models import StoredRequests
 
 
 class Ticket_5_Tests(TestCase):
-    def test_view_exist_for_edit_page(self):
-        "url edit/ must contain <form> tag"
-        response = self.client.get(reverse('edit:edit'))
+    def test_mockup(self):
+        """
+        Mockup of edit page should contain <form> tag and
+        based on edit_page.html
+        """
+        response = self.client.get(reverse('edit:edit_page'))
+        self.assertTemplateUsed(response, 'edit_page.html')
         self.assertContains(response, "form")
+
+    def test_view_with_hardcoded_data(self):
+        """
+        View must response with hard-coded data
+        """
+        response = self.client.get(reverse('edit:edit'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "form")
+        self.assertContains(response, "Dmytro")
+        self.assertContains(response, "Sapotnitskiy")
+
+    def test_view_used_template(self):
+        """
+        Test that view for index page return template hello.html in response
+        """
+        response = self.client.get(reverse('edit:edit'))
+        self.assertTemplateUsed(response, 'edit.html')
--- apps/edit/urls.py
+++ apps/edit/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
 from apps.edit import views
+from django.views.generic import TemplateView
 
 
-urlpatterns = patterns('',
-                       url(r'^$', views.edit, name="edit")
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.edit, name="edit"),
+    url(r'edit_page', TemplateView.as_view(template_name="edit_page.html"), name="edit_page")
+)
