d0161acb941770d1d1912dc70906fd4a49a7e267
dasap89 <shelbynagliy@gmail.com>
2017-03-12 15:45:07 +0200

    ticket:5 edit form tested and added

---
+++ apps/edit/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from apps.hello.models import Contact
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('name', 'last_name', 'date_of_birth', 'bio', 'email', 'jabber', 'skype',
+                  'other_contacts')
+'''
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_of_birth': forms.TextInput(attrs={'class': 'form-control'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'jabber': forms.EmailInput(attrs={'class': 'form-control'}),
+            'skype': forms.TextInput(attrs={'class': 'form-control'}),
+            'other_contacts': forms.Textarea(attrs={'class': 'form-control'})
+        }
+'''
---
+++ apps/edit/tests/test_forms.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+import datetime
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from ..forms import EditForm
+
+
+class Ticket_5_Tests_Form(TestCase):
+
+    def setUp(self):
+        self.form = EditForm
+        self.valid_form_data = {
+            'name': 'Dmytro',
+            'last_name': 'Sapotnitskiy',
+            'date_of_birth': datetime.date(year=1989, month=3, day=24),
+            'bio': 'Biography',
+            'email': 'email@email.com',
+            'jabber': 'jabber@jabber.com', 
+            'skype': 'some_skype',
+            'other_contacts': 'Here comes some other contacts',
+        }
+
+    def test_edit_form_with_no_data(self):
+        """
+        Test that EditForm can't be empty
+        """
+        self.assertFalse(self.form(data={}).is_valid(),
+                         'Should be invalid if no data is given')
+
+    def test_edit_form_with_invalid_data(self):
+        """
+        Test that EditForm is invalid when invalid data is
+        given
+        """
+        data = {'name': 'Dmytro', 'last_name': 'Sap'}
+        self.assertFalse(self.form(data=data).is_valid())
+
+    def test_edit_form_with_valid_data(self):
+        """
+        Test that EditForm is valid if sufficient data is
+        given
+        """
+        data = self.valid_form_data
+        self.assertTrue(self.form(data=data).is_valid())
